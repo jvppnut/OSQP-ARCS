0a29bb9483afb18d9a4cf5acae519dbc6f565d98
Juan <jvpadronp@gmail.com>
2024-09-09 23:38:36 +0900

    Created

---
+++ lib/OSQP-ARCS.hh
@@ -0,0 +1,18 @@
+//! @file OSQP-ARCS.hh
+//! @brief C++ ARCS wrapper for OSQP solver
+//!
+//! C++ wrapper for using OSQP solver on ARCS
+//!
+//! @date 2024/9/9
+//! @author Juan Padron
+//
+// Copyright (C) 2011-2024 Yokokura, Yuki
+// MIT License. For details, see the LICENSE file.
+
+
+#ifndef OSQPARCS
+#define OSQPARCS
+
+include <osqp.h>
+include "ArcsMatrix.hh"
+
